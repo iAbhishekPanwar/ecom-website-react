a6110e94521eea9927d93d18f82b3a75dd874bf8
Abhishek Panwar <imabhishek612@gmail.com>
2023-05-15 15:54:38 +0530

    responsive done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {
   Badge,
+  Button,
   Container,
   Dropdown,
   FormControl,
@@ -10,8 +11,15 @@ import {
 } from "react-bootstrap";
 import { FaShoppingCart } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { CartState } from "../context/Context";
+import { AiFillDelete } from "react-icons/ai";
 
 const Header = () => {
+  const {
+    state: { cart },
+    dispatch,
+    productDispatch,
+  } = CartState();
   return (
     <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
       <Container>
@@ -23,6 +31,12 @@ const Header = () => {
             style={{ width: 500 }}
             placeholder="Search a Product"
             className="m-auto"
+            onChange={(e) => {
+              productDispatch({
+                type: "FILTER_BY_SEARCH",
+                payload: e.target.value,
+              });
+            }}
           />
         </Navbar.Text>
         <Nav>
@@ -30,11 +44,75 @@ const Header = () => {
             <Dropdown.Toggle variant="success">
               <FaShoppingCart color="white" fontSize="25px" />
 
-              <Badge>{10}</Badge>
+              <Badge>{cart.length}</Badge>
             </Dropdown.Toggle>
 
+            {/* <Dropdown.Menu style={{ minWidth: 370 }}>
+              {cart.length>0?(
+                <>
+                {
+                  cart.map((prod) => (
+                    <img 
+                    src={prod.image}
+                    className="cartItemImg"
+                    alt={prod.name}
+                    />
+                    <div className="cartItemDetail">
+                      <span>{prod.name}</span>
+                      <span>₹ {prod.price.split('.')[0]}</span>
+                    </div>
+                    <AiFillDelete
+                    fontSize='20px'
+                    style={{cursor : 'pointer'}}
+                    onClick={() => {
+                dispatch({
+                  type: "REMOVE_FROM_CART",
+                  payload: prod,
+                });
+              }}
+              
+                    />
+                  ))
+                }
+                </>
+              ):(<span style={{ padding: 10 }}>Cart is Empty!</span>)}
+              
+            </Dropdown.Menu> */}
             <Dropdown.Menu style={{ minWidth: 370 }}>
-              <span style={{ padding: 10 }}>Cart is Empty!</span>
+              {cart.length > 0 ? (
+                <>
+                  {cart.map((prod) => (
+                    <div key={prod.id}>
+                      <img
+                        src={prod.image}
+                        className="cartItemImg"
+                        alt={prod.name}
+                      />
+                      <div className="cartItemDetail">
+                        <span>{prod.name}</span>
+                        <span>₹ {prod.price.split(".")[0]}</span>
+                      </div>
+                      <AiFillDelete
+                        fontSize="20px"
+                        style={{ cursor: "pointer" }}
+                        onClick={() => {
+                          dispatch({
+                            type: "REMOVE_FROM_CART",
+                            payload: prod,
+                          });
+                        }}
+                      />
+                    </div>
+                  ))}
+                  <Link to="/cart">
+                    <Button style={{ width: "95%", margin: "0 10px" }}>
+                      Go to Cart
+                    </Button>
+                  </Link>
+                </>
+              ) : (
+                <span style={{ padding: 10 }}>Cart is Empty!</span>
+              )}
             </Dropdown.Menu>
           </Dropdown>
         </Nav>
--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { Button, Card } from "react-bootstrap";
 import Rating from "./Rating";
+import { CartState } from "../context/Context";
 
 const SingleProduct = ({ prod }) => {
+  const {
+    state: { cart },
+    dispatch,
+  } = CartState();
+
   return (
     <div className="products">
       <Card>
@@ -18,10 +24,31 @@ const SingleProduct = ({ prod }) => {
             )}
             <Rating rating={prod.ratings} />
           </Card.Subtitle>
-          <Button variant="danger">Remove from cart</Button>
-          <Button disabled={!prod.inStock}>
-            {!prod.inStock ? "Out of Stock" : "Add to Cart"}
-          </Button>
+          {cart.some((p) => p.id === prod.id) ? (
+            <Button
+              onClick={() => {
+                dispatch({
+                  type: "REMOVE_FROM_CART",
+                  payload: prod,
+                });
+              }}
+              variant="danger"
+            >
+              Remove from cart
+            </Button>
+          ) : (
+            <Button
+              onClick={() => {
+                dispatch({
+                  type: "ADD_TO_CART",
+                  payload: prod,
+                });
+              }}
+              disabled={!prod.inStock}
+            >
+              {!prod.inStock ? "Out of Stock" : "Add to Cart"}
+            </Button>
+          )}
         </Card.Body>
       </Card>
     </div>
