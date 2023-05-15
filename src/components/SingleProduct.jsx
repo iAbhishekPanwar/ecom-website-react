2ed57de1b90559ec93b48e81a1f48ac434e63da9
Abhishek Panwar <imabhishek612@gmail.com>
2023-05-15 12:09:04 +0530

    fixed styling, added cart buttons

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Button, Card } from "react-bootstrap";
+import Rating from "./Rating";
 
 const SingleProduct = ({ prod }) => {
-  return <div>{prod.name}</div>;
+  return (
+    <div className="products">
+      <Card>
+        <Card.Img variant="top" src={prod.image} alt={prod.name} />
+        <Card.Body>
+          <Card.Title>{prod.name}</Card.Title>
+          <Card.Subtitle style={{ paddingBottom: 10 }}>
+            <span>₹ {prod.price.split(".")[0]}</span>
+            {prod.fastDelivery ? (
+              <div>Fast Delivery</div>
+            ) : (
+              <div>4 Days Delivery</div>
+            )}
+            <Rating rating={prod.ratings} />
+          </Card.Subtitle>
+          <Button variant="danger">Remove from cart</Button>
+          <Button disabled={!prod.inStock}>
+            {!prod.inStock ? "Out of Stock" : "Add to Cart"}
+          </Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 
 export default SingleProduct;
